2d86b28b84308e377c12891ef108d9073395ba00
FelipeEmanuel <felipe.nunes@ccc.ufcg.edu.br>
2022-10-18 19:31:31 -0300

    Primeira parte

---
+++ website/auth.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template,  request, flash
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be longer', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be longer', category='error')
+        elif password1 != password2:
+            flash('Senhas diferentes', category='error')
+        elif len(password1) < 7:
+            flash('Password must be longer', category='error')
+        else:
+            #add user to database
+            flash('Account created!', category='success')
+
+    return render_template("signup.html")
